e630be438be5d73cf3b135f907452bb38c393b92
SaravanaMK <saravanatweak@gmail.com>
2018-05-08 19:15:04 +0530

    Update RunningDeadLockCode.java

--- src/main/java/pluralSight/concurrency/threads/runnable/RunningDeadLockCode.java
+++ src/main/java/pluralSight/concurrency/threads/runnable/RunningDeadLockCode.java
@@ -1,8 +1,6 @@
 package pluralSight.concurrency.threads.runnable;
 
 
-import pluralSight.concurrency.threads.model.DeadLockCode;
-
 public class RunningDeadLockCode {
 
 	public static void main(String[] args) throws InterruptedException {
@@ -32,8 +30,37 @@ public class RunningDeadLockCode {
 
 		Thread t2 = new Thread(r2);
 		t2.start();
-
+/*;
 		t1.join();
-		t2.join();
+		t2.join();*/
+	}
+}
+
+class DeadLockCode {
+	//Run the below code below in debug mode and try to see the threads holding
+	//dead lock keys alternatively . If you couldn't do in Intellij follow the below link for eclipse
+	//https://app.pluralsight.com/player?course=java-patterns-concurrency-multi-threading&author=jose-paumard&name=java-patterns-concurrency-multi-threading-m1&clip=16&mode=live
+
+	private Object key1 = new Object();
+	private Object key2 = new Object();
+
+	public void a() {
+		synchronized (key1) {
+			System.out.println("[" + Thread.currentThread().getName() + "] I am in a()");
+			b();
+		}
+	}
+
+	public void b() {
+		synchronized (key2) {
+			System.out.println("[" + Thread.currentThread().getName() + "] I am in b()");
+			c();
+		}
+	}
+
+	public void c() {
+		synchronized (key1) {
+			System.out.println("[" + Thread.currentThread().getName() + "] I am in c()");
+		}
 	}
 }
